a6046b6c564beebc30ab78c60d252725a2d90767
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-22 06:23:17 +0900

    create delete.html update.html

--- myapp/diary/urls.py
+++ myapp/diary/urls.py
@@ -9,4 +9,6 @@ urlpatterns =[
     path('diary/create/complete/', views.DiaryCreateCompleteView.as_view(), name='diary_create_complete'),
     path('diary/list/', views.DiaryListView.as_view(), name='diary_list'),
     path('diary/detail/<uuid:pk>/', views.DiaryDetailView.as_view(), name='diary_detail'),
+    path('diary/update/<uuid:pk>/', views.DiaryUpdateView.as_view(), name='diary_update'),
+    path('diary/delete/<uuid:pk>/', views.DiaryDeleteView.as_view(), name='diary_delete'),
 ]
--- myapp/diary/views.py
+++ myapp/diary/views.py
@@ -1,9 +1,10 @@
 from re import template
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import DiaryForm
 from .models import Diary
+from django.utils import timezone
 
 
 
@@ -27,3 +28,20 @@ class DiaryListView(ListView):
 class DiaryDetailView(DetailView):
     template_name = 'diary_detail.html'
     model = Diary
+
+class DiaryUpdateView(UpdateView):
+    template_name = 'diary_update.html'
+    model = Diary
+    fields = ('date', 'title', 'text',)
+    success_url = reverse_lazy('diary:diary_list')
+
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.update_at = timezone.now()
+        diary.save()
+        return super().form_valid(form)
+
+class DiaryDeleteView(DeleteView):
+    template_name = 'diary_delete.html'
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
