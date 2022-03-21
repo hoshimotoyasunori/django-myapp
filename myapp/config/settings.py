68f5d2ae4e63e8d227bbdc2063cdd389212c835c
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-21 09:30:21 +0900

    create mysqldatabase

--- myapp/config/settings.py
+++ myapp/config/settings.py
@@ -76,12 +76,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql', # 変更
+#         'NAME': 'django_app', # プロジェクトで使用するデータベース名
+#         'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+#         'PASSWORD': 'root', # 同上。そのパスワード
+#     }
+# }
 
 
 # Password validation
