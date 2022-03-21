56e6331c975faad0569ebe8c974f934dcded31e2
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-21 13:05:03 +0900

    create supereuser

--- myapp/diary/models.py
+++ myapp/diary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+
+
+class  Diary(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(verbose_name='日付', default=timezone.now)
+    title = models.CharField(verbose_name='タイトル', max_length=40)
+    text = models.CharField(verbose_name='本文', max_length=40)
+    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name='編集日時', blank=True, null=True)
+
